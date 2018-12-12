cacaeb25fddf344e29f6135c686c748015a4fde3
adexphem <adexphem4real@gmail.com>
2018-12-12 22:06:25 +0100

    add links and header for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Navbar/>
+    <div class="container main">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Navbar
+  }
 };
 </script>
 
@@ -15,5 +23,9 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.main {
+  font-size: 14px;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,9 @@
 <template>
-  <div id="navbar">NavBar</div>
+  <nav>
+    <div class="nav-wrapper green">
+      <div class="container">
+        <router-link to="/" class="brand-logo">Kontacts App</router-link>
+      </div>
+    </div>
+  </nav>
 </template>
