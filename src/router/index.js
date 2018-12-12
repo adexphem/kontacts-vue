91049328f64c5a9eaf6982e9de28facd023b107e
adexphem <adexphem4real@gmail.com>
2018-12-12 21:43:06 +0100

    adding needed assets libraries e.g font-awesome, materialise e.t.c

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Dashboard from '@/components/Dashboard';
+import EditMember from '@/components/EditMember';
+import ViewMember from '@/components/ViewMember';
+import NewMember from '@/components/NewMember';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/new',
+      name: 'new-member',
+      component: NewMember
+    },
+    {
+      path: '/edit/:member_id',
+      name: 'edit-member',
+      component: EditMember
+    },
+    {
+      path: '/:member_id',
+      name: 'view-member',
+      component: ViewMember
     }
   ]
-})
+});
